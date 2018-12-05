189197d5af0cf5bbcf6ba25c0d553cf2f3e55afb
jars <j.98.alejandro@gmail.com>
2018-12-04 23:27:26 -0600

    Se agrego codigo en el modelo.dao para cada clase..

--- PROGRA4_MOVILES/src/java/modelo/dao/ModelosDao.java
+++ PROGRA4_MOVILES/src/java/modelo/dao/ModelosDao.java
@@ -5,10 +5,76 @@
  */
 package modelo.dao;
 
-/**
- *
- * @author Ismael
- */
+import java.util.List;
+import modelo.entidad.Modelo;
+import modelo.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.Query;
+
 public class ModelosDao {
-    
+    public List<Modelo> listarModelo() {
+        List<Modelo> lista = null;
+        Session sesion = HibernateUtil.getSessionFactory().openSession();
+        Transaction t = sesion.beginTransaction();
+        String hql = "FROM Modelo";
+        try {
+            lista = sesion.createQuery(hql).list();
+            t.commit();
+            sesion.close();
+        } catch (Exception e) {
+            t.rollback();
+        }
+        return lista;
+    }
+    public void agregar(Modelo modelo) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.save(modelo);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+
+    public void modificar(Modelo modelo) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.update(modelo);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+
+    public void eliminar(Modelo modelo) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.delete(modelo);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
 }
--- PROGRA4_MOVILES/src/java/modelo/dao/StockDao.java
+++ PROGRA4_MOVILES/src/java/modelo/dao/StockDao.java
@@ -5,10 +5,76 @@
  */
 package modelo.dao;
 
-/**
- *
- * @author Ismael
- */
+import java.util.List;
+import modelo.entidad.Stock;
+import modelo.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.Query;
+
 public class StockDao {
-    
+    public List<Stock> listarStock() {
+        List<Stock> lista = null;
+        Session sesion = HibernateUtil.getSessionFactory().openSession();
+        Transaction t = sesion.beginTransaction();
+        String hql = "FROM Stock";
+        try {
+            lista = sesion.createQuery(hql).list();
+            t.commit();
+            sesion.close();
+        } catch (Exception e) {
+            t.rollback();
+        }
+        return lista;
+    }
+    public void agregar(Stock stock) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.save(stock);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+
+    public void modificar(Stock stock) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.update(stock);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+
+    public void eliminar(Stock stock) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.delete(stock);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
 }
